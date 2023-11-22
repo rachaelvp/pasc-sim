ff5f4af6211e93327695ac7033a7c16ba685afc6
Jeremy Coyle <jeremyrcoyle@gmail.com>
2023-11-22 11:41:21 -0800

    fix desc

--- tests/testthat/test-stremr.R
+++ tests/testthat/test-stremr.R
@@ -4,7 +4,9 @@ library(truncnorm)
 library(data.table)
 library(zoo)
 library(stremr)
-data <- generate_data(1e3)
+sampled_data <- generate_data(1e3)
+data <- sampled_data$study_tv
+# data
 tmle_est <- fit_TMLE(OData, tvals = tvals, intervened_TRT = "TI.set1", Qforms = Qforms, parallel = TRUE)
 
 
