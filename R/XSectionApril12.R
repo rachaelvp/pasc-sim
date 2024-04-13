e2f5f2346c12520aa2fe7f0edd39c4c86650a45f
Alan Hubbard <hubbard@berkeley.edu>
2024-04-12 17:55:21 -0700

    april12

--- R/XSectionApril12.R
+++ R/XSectionApril12.R
@@ -8,10 +8,8 @@ library(tmle3)
 library(hal9001)
 
 rm(list = ls())
-source("uhal_Alan.R")
+source("uhal_Alan_2.R")
 source("SimSynFunctions_Alan.R")
-
-
 
 
 # Initiate data, get true values
@@ -56,7 +54,7 @@ learner_list <- list(A = sl_A, Y = sl_Y)
 # HAL spec  
 ### This name needs to be used
 lrnr_uhal <- Lrnr_uhal9001$new()
-
+lrnr_uhalcv <- Lrnr_uhal9001cv$new()
 
 # Set up Sim
 ## Specs for SL and HAL and TMLE 
@@ -64,7 +62,7 @@ lrnr_uhal <- Lrnr_uhal9001$new()
 # B is number of simulations
 tmle.res <- NULL
 simp.res <- NULL
-B<- 400
+B<- 3
 library(foreach)
 library(doParallel)
 num_cores <- 7
@@ -74,21 +72,35 @@ foreach(b = 1:B) %do% {
   
 # Simulate Data
 df <- generate_data_simple(N=500)
-# Fit HAL Q and g
+# Fit undersmoothed HAL Q and g
 res_Qg <- fit_uhal_Qg(df = df, 
                       y_type = "continuous", 
                       covars = node_list$W)
+# Fit CV HAL Q and g
+res_Qgcv <- fit_uhal_Qg_cv(df = df, 
+                      y_type = "continuous", 
+                      covars = node_list$W)
+
 # Fit SL Q and g
 res_Qg_SL <- fit_SL_Qg(df = df, 
                       y_type = "continuous", 
                       covars = node_list$W)
 
-# Get HAL-generated synthetic data
+# Get undersmoothed HAL-generated synthetic data
 df_syn <- generate_uhal_data(n = nrow(df), 
                              df = df, 
                              y_type = "continuous", 
                              g_fit = res_Qg$g_fit, 
                              Q_fit = res_Qg$Q_fit)
+
+# Get CV HAL-generated synthetic data
+df_syn_cv <- generate_uhal_data(n = nrow(df), 
+                             df = df, 
+                             y_type = "continuous", 
+                             g_fit = res_Qgcv$g_fit, 
+                             Q_fit = res_Qgcv$Q_fit)
+
+
 
 # Get SL-generated synthetic data
 df_syn_SL <- generate_SL_data(n = nrow(df), 
@@ -100,23 +112,29 @@ df_syn_SL <- generate_SL_data(n = nrow(df),
 # Get the empirical estimates of simple parameters
 simple.df <- get.estimates(df)
 simple.df.hal <- get.estimates(df_syn)
+simple.df.hal_cv <- get.estimates(df_syn_cv)
 simple.df.SL <- get.estimates(df_syn_SL)
 
 
 # estimate ATE with original data
 tmle_fit_org <- tmle3(ate_spec, df, node_list, learner_list)
 
-# estimate ATE with synthetic HAL data
+# estimate ATE with synthetic undersmoothed HAL data
 tmle_fit_hal <- tmle3(ate_spec, df_syn, node_list, learner_list)
 
-# estimate ATE with synthetic HAL data
+# estimate ATE with synthetic cv HAL data
+tmle_fit_hal_cv <- tmle3(ate_spec, df_syn_cv, node_list, learner_list)
+
+
+
+# estimate ATE with synthetic SL data
 tmle_fit_SL <- tmle3(ate_spec, df_syn_SL, node_list, learner_list)
 
 # Consolidate into a output objects
-simp.res <- rbind(simp.res,c(simple.df$aver,simple.df.hal$aver,simple.df.SL$aver,simple.df$reg[,1],simple.df$reg[,2],simple.df.hal$reg[,1],simple.df.hal$reg[,2],simple.df.SL$reg[,1],simple.df.SL$reg[,2]))
+simp.res <- rbind(simp.res,c(simple.df$aver,simple.df.hal$aver,simple.df.hal_cv$aver,simple.df.SL$aver,simple.df$reg[,1],simple.df$reg[,2],simple.df.hal$reg[,1],simple.df.hal$reg[,2],simple.df.hal_cv$reg[,1],simple.df.hal_cv$reg[,2],simple.df.SL$reg[,1],simple.df.SL$reg[,2]))
 
-tmle.res <- rbind(tmle.res,c(tmle_fit_org$summary$tmle_est,tmle_fit_org$summary$se,tmle_fit_hal$summary$tmle_est,tmle_fit_hal$summary$se,tmle_fit_SL$summary$tmle_est,tmle_fit_SL$summary$se))
+tmle.res <- rbind(tmle.res,c(tmle_fit_org$summary$tmle_est,tmle_fit_org$summary$se,tmle_fit_hal$summary$tmle_est,tmle_fit_hal$summary$se,tmle_fit_hal_cv$summary$tmle_est,tmle_fit_hal_cv$summary$se,tmle_fit_SL$summary$tmle_est,tmle_fit_SL$summary$se))
 
 }
-save.image("April7.rdata")
+save.image("April12.rdata")
 
